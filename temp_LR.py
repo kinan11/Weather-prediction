ae1cf74f797be5a8c2fe3ae283698ec4f02631a7
kinan11 <kingamiszczak1@op.pl>
2022-10-22 15:43:52 +0200

    LR

--- temp_LR.py
+++ temp_LR.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
@@ -7,19 +8,30 @@ import numpy as np
 
 def main():
     data = pd.read_csv('Dataset.csv')
+    data = data.drop('Dzień', axis=1)
+    data = data.drop('Miesiąc', axis=1)
+    data = data.drop('Rok', axis=1)
 
     # Pozyskanie danych
-    x_tmp = data.iloc[:, 6].values
-    y = data.iloc[5:, 6].values
+    x_tmp = data.iloc[:, 1:].values
+    y = data.iloc[4:, 3].values
 
     x = []
-    for i in range(len(x_tmp)-5):
-        x.append([x_tmp[i], x_tmp[i+1], x_tmp[i+2], x_tmp[i+3], x_tmp[i+4]])
+    for i in range(len(x_tmp) - 4):
+        x.append([])
+        x[i].extend(x_tmp[i])
+        x[i].extend(x_tmp[i+1])
+        x[i].extend(x_tmp[i + 2])
+        x[i].extend(x_tmp[i + 3])
 
     # Podział na dane uczące i trenujące
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-    # Regresja liniowa
+    # Skalowanie danych
+    st_x = StandardScaler()
+    x_train = st_x.fit_transform(x_train)
+    x_test = st_x.transform(x_test)
+
     regressor = LinearRegression()
     regressor.fit(x_train, y_train)
 
